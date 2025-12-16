5b80707eb3025e41f0780ad9b947ceaae8b35f06
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2025-12-16 11:25:38 +0530

    userSchema has been created successfully.

---
+++ src/models/userSchema.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
+
+const userSchema = new mongoose.Schema(
+    {
+        name: { type: String, required: true },
+        email: { type: String, required: true, unique: true },
+        password: { type: String, required: true },
+        role: { type: String, enum: ["admin", "doctor", "patient"], default: "patient" },
+    },
+    { timestamps: true }
+);
+
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+
+userSchema.methods.comparePassword = function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+
+userSchema.methods.generateToken = function () {
+    return jwt.sign({ _id: this._id, role: this.role }, process.env.JWT_SECRET, { expiresIn: "7d" });
+};
+
+
+export const User = mongoose.model("User", userSchema);
