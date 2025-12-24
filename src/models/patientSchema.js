49497ced78c41ba6292c06473e6b5137ac0eca2d
Sufiyan Siddique <mdsufiyanahmad325@gmail.com>
2025-12-24 11:30:16 +0530

    fix some bugs

--- src/models/patientSchema.js
+++ src/models/patientSchema.js
@@ -2,7 +2,9 @@ import mongoose from "mongoose";
 
 const patientSchema = new mongoose.Schema(
   {
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    user_id: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+
+    name: { type: String, required: true },
 
     age: { type: Number, required: true },
 
